9029640e426c4e88a5f0992b8c7b6f75c37a31f6
tarun_kumar <114987231+Tarunkumarsaadhu@users.noreply.github.com>
2023-02-11 20:35:26 +0530

    Add files via upload

---
+++ BBMS/BBMS/index.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blood Bank Management</title>
+    <!-- Bootstrap files -->
+    <link rel="stylesheet" href="bootstrap/css//bootstrap.min.css">
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+    <!-- External CSS file -->
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-danger">
+        <a class="navbar-brand" href="index.php">Blood Bank Management System</a>
+       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="index.php">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="admin/login.php">Admin</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="donor/login.php">Donor</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="patient/login.php">Patient</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="mailto:vijaymudiraj2@gmail.com" >Contacts us</a> 
+           
+            </li>
+            </ul>
+        </div>
+    </nav>
+<!-- Main content -->
+<div class="container-fluid" style="margin:50px;">
+    <div class="row container" style="text-align: justify;">
+        <div class="col-md-8 mx-auto content-left">
+            <h4> About blood bank mangement </h4>
+            <p>Every two seconds, someone in the India needs blood. Blood is essential to help patients survive surgeries, cancer treatment, chronic illnesses, and traumatic injuries. This lifesaving care starts with one person making a generous donation. The need for blood is constant. But only about 3% of age-eligible people donate blood yearly. You can help the Indian Red Cross make sure that supply meets demand.
+                 </p>
+            <button class="btn btn-danger">Read more..</button>
+        </div>
+        <div class="col-md-3 content-right">
+            <img class="img-fluid" src="../BBMS/images/image1.jpg" height="auto" width="70%">
+        </div>
+    </div>
+</div>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-10 m-auto" style="text-align: justify;">
+            <h4>Why should people donate blood?</h4>
+            <p> 
+            Safe blood saves lives. Blood is needed by women with complications during pregnancy and childbirth, children with severe anaemia, often resulting from malaria or malnutrition, accident victims and surgical and cancer patients.
+
+There is a constant need for a regular supply of blood because it can be stored only for a limited period of time before use. Regular blood donation by a sufficient number of healthy people is needed to ensure that blood will always be available whenever and wherever it is needed.
+
+Blood is the most precious gift that anyone can give to another person – the gift of life. A decision to donate your blood can save a life, or even several if your blood is separated into its components – red cells, platelets and plasma – which can be used individually for patients with specific conditions.</p>
+        <!--    <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facere, corrupti fugiat alias voluptatum, cupiditate dolorem fuga laborum perferendis eos laboriosam animi ullam libero doloremque accusamus optio, laudantium nesciunt exercitationem nam. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facere, corrupti fugiat alias voluptatum, cupiditate dolorem fuga laborum perferendis eos laboriosam animi ullam libero doloremque accusamus optio, laudantium nesciunt exercitationem nam.
+            </p> -->
+        </div>
+    </div>
+</div>
+<div class="container-fluid" style="margin-bottom: 5%;">
+    <div class="row">
+        <div class="col-10  m-auto">
+        <h4> Our camps ...! </h4>
+        <div class="card container">
+            <h4>MRCET</h4>
+            <hr>
+            <p>Total camps conducted : 10 <br> Total Donations : 100+ <br> people benefitted : 90+</p>
+        </div>
+        <div class="card">
+            <h4>MREC</h4>
+            <hr>
+            <p>Total camps conducted : 09 <br> Total Donations : 100+ <br> people benefitted : 80+</p>
+        </div>
+        <div class="card">
+            <h4>MRECW</h4>
+            <hr>
+            <p>Total camps conducted : 11 <br> Total Donations : 200+ <br> people benefitted : 110+</p>
+        </div>
+        <div class="card">
+            <h4>MRU</h4>
+            <hr>
+            <p> Total camps conducted : 04 <br> Total Donations : 50+ <br> people benefitted : 40+</p>
+        </div>
+        </div>
+    </div>
+</div>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-12 bg-danger footer">
+            &copy 2023 CSE -D11
+            
+
+        </div>
+    </div>
+</div>
+
+</body>
+</html>
